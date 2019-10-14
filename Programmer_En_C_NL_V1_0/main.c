ace23da943f5731b21eb8d2a8ff0d4aa0379ad89
nglconsulting <nico.nglconsulting@gmail.com>
2019-10-14 16:59:38 +0200

    Structure d'un programme en C

--- Programmer_En_C_NL_V1_0/main.c
+++ Programmer_En_C_NL_V1_0/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define NFOIS 5
+
+//Exemple de programme en C
+
+
 
 int main()
 {
-    printf("Hello world!\n");
+    int i;
+    float x;
+    float racx;
+
+
+    printf("Bonjour\n");
+
+
+    printf("Je vais vous calculer %d racines carrees\n",NFOIS);
+
+    for(i=0;i<NFOIS;i++){
+        printf("Donnez un nombre : ");
+        scanf("%f",&x);
+        if(x<0.0)
+            printf("Le nombre %f ne possède pas de racine carree\n",x);
+        else{
+            racx=sqrtf(x);
+            printf("le nombre %f a pour racine carree : %f\n",x,racx);
+        }
+    }
+
+    printf("Travail Termine - Au revoir");
+
+
     return 0;
 }
