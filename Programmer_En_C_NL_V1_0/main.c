c21c1516e0518720855b1a127e2305aa8305927c
nglconsulting <nico.nglconsulting@gmail.com>
2019-10-15 10:55:55 +0200

    Les types Les contantes (Macro) Les printf Les operateurs

--- Programmer_En_C_NL_V1_0/main.c
+++ Programmer_En_C_NL_V1_0/main.c
@@ -2,37 +2,75 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define NFOIS 5
+#define NFOIS 5 //constante
+#define PI 3.14
+#define NOM "Lehmann"
+#define CAR 'x'
 
 //Exemple de programme en C
 
+//Commentaires lignes
 
+/* Commentaires paragraphes*/
 
 int main()
 {
-    int i;
-    float x;
-    float racx;
+    //Integer
+    short int nA; //short sn
+    int nB;
+    long int nC; //long ln
 
 
-    printf("Bonjour\n");
+    printf("Taille de a : %d\n",sizeof(nA));
+    printf("Taille de b : %d\n",sizeof(nB));
+    printf("Taille de c : %d\n",sizeof(nC));
 
 
-    printf("Je vais vous calculer %d racines carrees\n",NFOIS);
+    //Real
+    float fltReelSimple;
+    double dblReelDouble;
+    long double ldblReel=10.2580;
 
-    for(i=0;i<NFOIS;i++){
-        printf("Donnez un nombre : ");
-        scanf("%f",&x);
-        if(x<0.0)
-            printf("Le nombre %f ne possède pas de racine carree\n",x);
-        else{
-            racx=sqrtf(x);
-            printf("le nombre %f a pour racine carree : %f\n",x,racx);
-        }
-    }
+     printf("Taille de fltReelSimple : %d\n",sizeof(fltReelSimple));
+     printf("Taille de dblReelDouble : %d\n",sizeof(dblReelDouble));
+     printf("Taille de ldblReel : %d\n",sizeof(ldblReel));
+     printf("Taille de val de ldblReel : %lf\n",(float)ldblReel);
 
-    printf("Travail Termine - Au revoir");
+
+
+     //caractères
+
+     char cCaractere;
+     printf("Taille de cCaractere : %d\n",sizeof(cCaractere));
+
+
+    //Tester Define
+     printf("NFOIS : %d\n",NFOIS);
+     printf("PI : %f\n",PI);
+     printf("NOM : %s\n",NOM);
+     printf("CAR : %c\n",CAR);
+
+     //Opérateurs
+     int nI=0;
+     ++nI; //pre-incrementation
+     nI++; //post-incremenation
+     // % modulo
+     /*  / division */
+
+     //Cast
+     long double ldblLongDouble=105.8933259635;
+     printf("Val de ldblLongDouble : %lf",(double)ldblLongDouble);
+
+
+
+
 
 
     return 0;
 }
+
+
+
+
+
+
