bb600473f4aaaed7d989faa7f40eecfed82ae5ce
nglconsulting <nico.nglconsulting@gmail.com>
2019-10-21 17:52:14 +0200

    //Les entrées sorties conversationnelles Printf scanf getchar getc gets fgets fgetc Debug step by step Formatage à l'entrée et la sortie

--- Programmer_En_C_NL_V1_0/main.c
+++ Programmer_En_C_NL_V1_0/main.c
@@ -3,112 +3,53 @@
 #include <math.h>
 
 #define TAILLE 10
+#define MOT "HELLO"
 
-//Strcutures Conditions et boucles
-
+//Les entrées sorties conversationnelles
 
 
 int main()
 {
 
-    int a=0,b=0,c=0, d=0;
+    //Affichage
+    printf("%d\n",TAILLE);
+    printf("%s\n",MOT);
 
-    //Condition
 
-    if(a==b){
-        printf("Ils sont egaux\n");
-    }else{
-        printf("Ils sont diff\n");
+    //Saisie
+
+    int nVal;
+    scanf("%d",&nVal);
+    fflush(stdin);//vide le buffer de la saisie
+    char cCar;
+    scanf("%c",&cCar);
+    fflush(stdin);
+    char tab_cCar[TAILLE]; //de 0 a TAILLE-1
+    scanf("%s",tab_cCar);
+    gets(tab_cCar); //ou
+    printf("%s",tab_cCar);
+
+
+
+
+    printf("Entrez un caractère\n");
+    cCar=getchar();
+    while((cCar>='a' && cCar<='z')|| (cCar>='A' && cCar<='Z')){
+
+        printf("%c\n",cCar);
+        fflush(stdin);
+        cCar=getchar();
     }
 
-    //Condition avec operateurs logiques
-
-    if((a==b) && (a==c)){
-        printf("Ils sont tous egaux\n");
-
-    }else{
-        printf("Ils sont différents\n");
-    }
 
 
-    if((a==b) || (a==c)){
-        printf("Ils sont egaux\n");
-    }else{
-        printf("Ils sont diff\n");
-    }
+    //Saisie avec formatage de l'entrée et la sortie
 
-    if((a==b) || (a==c)){
-        printf("Ils sont egaux\n");
-    }else if((a==c)&& (a!=d)){
-        printf("blabla...\n");
-    }
-
-
-    //Boucle
-    int i=0;
-
-    for(i=0;i<TAILLE;i++){
-
-        printf("Hello world\n");
-    }
-
-    //TQ
-
-    i=0;
-    while(i<TAILLE){
-
-         printf("Hello world\n");
-         i++;
-
-    }
-
-    //Faire TQ
-
-    i=0;
-    do{
-        printf("Hello world\n");
-        i++;
-    }while(i<TAILLE);
-
-
-    //Cas alternatif de la condition
-
-    int choix=-1;
-
-    printf("Veuillez entrer votre choix\n");
-    scanf("%d",&choix);
-    switch(choix){
-            case 0:printf("Quitter\n");break;
-            case 1:printf("calculer\n");break;
-            case 2:printf("Jouer\n");break;
-            default : printf("default\n");break;
-
-    }
-
-    //test ? instruction1 : instruction 2
-    a==b? printf("Egaux"): printf("Diff");
-
-
-    //Instruction break;
-    for(i=0;i<TAILLE;i++){
-        printf("Hello world\n");
-
-        if(i==3){
-            break;
-        }
-    }
-
-  //Instruction continue;
-    for(i=0;i<TAILLE;i++){
-        printf("Hello world\n");
-
-        if(i<3){
-            printf("continue\n");
-            continue;
-
-        }
-    }
-
+    float fltReal=123.14921568413;
+    int nInteger=0;
+    printf("Entrez un reel positif ou negatif sur 2 digits");
+    scanf("%2d",&nInteger);
+    printf("%2.3f",fltReal);
 
 
     return 0;
