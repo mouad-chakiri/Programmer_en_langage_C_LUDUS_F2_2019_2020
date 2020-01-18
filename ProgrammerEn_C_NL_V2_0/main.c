5f10b762af87a50b109fea6631d0a2d016275174
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-01-18 09:57:15 +0100

    MakeFile, Cmake Pointeurs Génériques Pointeurs sur fonctions Pointeurs sur fonctions en argument Static inline

--- ProgrammerEn_C_NL_V2_0/main.c
+++ ProgrammerEn_C_NL_V2_0/main.c
@@ -5,77 +5,95 @@
 #define TAILLE 100
 
 
-//Gestion dynamique de la memoire
 
-void* initTabEntier(int *pnEntier);
+
+
+
+//Pointeurs genriques
+void raz(void *adr,int n);
+
+
+//Pointeurs sur des fonctions
+void (*adf)(double,int);
+void fct1(double,int);
+
+//Pointeurs sur des fonction transmises en argument
+int fct2(void(*adf)(double,int));
+
+//Méthode staique en ligne
+static inline void echange(int *nVal1, int *nVal2);
 
 
 int main()
 {
-     //malloc et free
-   //int tab[1000000000]; (tab too large)
-
-   /*int *pTabEntier=(int*)malloc(10000000*sizeof(int));
-
-    int i;
-    for(i=0;i<10;i++){
-        pTabEntier[i]=0;
-    }
 
 
-   free(pTabEntier);*/
+    //Utilisation pointeurs generiques
+    double z;
+    raz(&z,sizeof(z));
 
+    printf("%lf",z);
 
+    int tab[10];
 
-//calloc et realloc
-
-
-   /* int *pTabEntier=NULL;
-    int *pTabEntier2=NULL;
-
-    pTabEntier=(int*)calloc(10,sizeof(int));
-
-    pTabEntier2=(int*)realloc(pTabEntier,100*sizeof(int));
-
-
-    int i;
-    for(i=0;i<100;i++){
-        pTabEntier2[i]=5;
-    }
-
-    free(pTabEntier2);*/
+    raz(tab,10*sizeof(int));
 
 
 
-    int *pTabEntier=(int*)calloc(1,sizeof(int));
+    //Pointeurs sur des fonctions
+	adf=fct1;  //Avec ou Sans & si pas & il assigne la ref automatiquement
 
-    int *pTabEntier2=initTabEntier(pTabEntier);
-
-
-    int i;
-    for(i=0;i<10;i++){
-        pTabEntier2[i]=5;
-        printf("%d\n",pTabEntier2[i]);
-    }
-    free(pTabEntier);
-    free(pTabEntier2);
+    (*adf)(2.3,10);
 
 
+  //Pointeurs sur des fonction transmises en argument
+    fct2(fct1);
 
+    //Méthode staique en ligne
+    int nN1=10;
+    int nN2=20;
+    echange(&nN1,&nN2);
 
 
    return 0;
 }
 
 
-void* initTabEntier(int *pnEntier){
 
 
-    pnEntier=(int*)realloc(pnEntier,10*sizeof(int));
+//Pointeurs Génériques
 
-    return (void*)pnEntier;
-
+void raz(void *adr,int n){
+    int i;
+    char *ad=adr;
+    for(i=0;i<n;i++,ad++)
+        *ad=0;
 
 }
 
 
+//Pointeurs sur des fonctions
+void fct1(double a,int b){
+
+    printf("%lf,%d\n",a,b);
+
+}
+
+//Pointeurs sur des fonctions transmises en argument
+int fct2(void(*adf)(double,int)){
+
+    (*adf)(2.3,10);
+
+    return 0;
+}
+
+//Méthode staique en ligne
+static inline void echange(int *nVal1, int *nVal2){
+
+    int nTemp;
+
+    nTemp=*nVal1;
+    *nVal1=*nVal2;
+    *nVal2=nTemp;
+
+}
