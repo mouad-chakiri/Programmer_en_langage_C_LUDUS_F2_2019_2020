86e5e89a4401549e3917655b9813af1c46373c6d
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-01-27 17:46:28 +0100

    SDL2 Documentation et wiki Type SDL2(Structures) Initialisation SDL Création des instances Window et renderer Fcts         SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);         SDL_RenderPresent(pRenderer);         SDL_Delay(2000);         SDL_RenderClear(pRenderer);
            SDL_DestroyRenderer(pRenderer);
            SDL_DestroyWindow(pWindow);
            SDL_Quit();

--- ProgrammerEn_C_NL_V2_0/main.c
+++ ProgrammerEn_C_NL_V2_0/main.c
@@ -1,32 +1,53 @@
 #include<stdio.h>
-#include"SDL2_Display.h"
+#include "SDL2/SDL.h"
 
 int main(int argc, char *argv[]){
 
-    int i;
-    for(i=0; i<argc;i++){
-        printf("%s\n",argv[i]);
-
-    }
-
-    pWindow=NULL;
-    pRenderer=NULL;
-    ptexture=NULL;
-    psurface=NULL;
 
 
-     //initialize SDL
+     SDL_Window *pWindow=NULL;//Pointeur typedef SDL_Window possede la reference d'un window(Win32)
+     SDL_Renderer *pRenderer=NULL;//Pointeur typedef SDL_Renderer possede la reference du rendu encapsule dans Window
+     SDL_Texture *pTexture=NULL;//Pointeur typdef SDL_Texture possede la reference l'objet image,materiel ou texture qui sera blite dans le renderer(Rendu)
 
+
+    //initialisation de SDL
     if(SDL_Init(SDL_INIT_EVERYTHING)>=0)
     {
-            //if succeeded create our window
-            pWindow=SDL_CreateWindow("SDL_Application",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,780,SDL_WINDOW_SHOWN);
-            //if succeeded create window, create our render
+            //Creer le pointeur sur la reference de la fenetre Win32
+            pWindow=SDL_CreateWindow("SDL_Application",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,780
+                                     ,SDL_WINDOW_SHOWN);
+            //Si la fenetre creee
             if(pWindow!=NULL){
-                pRenderer=SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_PRESENTVSYNC);
-
+                //Creation du rendu encapsule dans la fenetre pointee par pWindow
+                pRenderer=SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
 
             }
+
+        //Traitement
+        SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderPresent(pRenderer);
+        //
+
+        //Synchronisation et rafraichissement d'affichage des frames à l'ecran
+        SDL_Delay(2000);
+
+        //Set de la couleur du traitement
+        SDL_SetRenderDrawColor(pRenderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+        //Effecement de l'etat precedent du renderer
+        SDL_RenderClear(pRenderer);
+        //Affiche l'etat du nouveau rendu
+        SDL_RenderPresent(pRenderer);
+
+
+        //Attente et fin du programme
+        SDL_Delay(10000);
+
+        //suppression et nettoyage des instances
+        SDL_DestroyRenderer(pRenderer);
+        SDL_DestroyWindow(pWindow);
+        SDL_Quit();
+
+
 
 
     }else{
@@ -35,7 +56,11 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    SDL_Delay(10000);
+
+
 
 return 0;
 }
+
+
+
